d7afe98c5fbd2cd68b1f0599a3e7f90a66d98236
phamviethoa <phamviethoa2416@gmail.com>
2026-02-02 22:13:43 +0700

    refactor: group request DTOs by auth, user, and admin contexts

--- src/main/kotlin/com/example/cargotracking/modules/user/model/dto/request/RefreshTokenRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cargotracking.modules.user.model.dto.request
-
-import jakarta.validation.constraints.NotBlank
-
-data class RefreshTokenRequest(
-    @field:NotBlank(message = "Refresh token is required")
-    val refreshToken: String
-)
-
